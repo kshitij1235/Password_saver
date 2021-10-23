ec6e3715f3ca19e6a57ae97f19cb48bbb13a62e9
Kshitij <65331304+kshitij1235@users.noreply.github.com>
2021-10-23 15:31:28 +0530

    removed non woking features

--- source_code/password.py
+++ source_code/password.py
@@ -20,14 +20,14 @@ def read_file(filename):
         pass
 
 
-def encrypt_data(data):
-    from cryptography.fernet import Fernet
-    key = Fernet.generate_key()
-    fernet = Fernet(key)
-    enc_message = fernet.encrypt(data.encode())
-    print(enc_message)
-    # decMessage = fernet.decrypt(encMessage).decode()
-    # print("decrypted string: ", decMessage)
+# def encrypt_data(data):
+#     from cryptography.fernet import Fernet
+#     key = Fernet.generate_key()
+#     fernet = Fernet(key)
+#     enc_message = fernet.encrypt(data.encode())
+#     print(enc_message)
+#     # decMessage = fernet.decrypt(encMessage).decode()
+#     # print("decrypted string: ", decMessage)
 
 
 def display_passwords():
@@ -42,19 +42,19 @@ def display_passwords():
 
     output.insert(END, read_file('data.txt'))
 
-def forget_password():
-    forget_password_window = Tk()
-    forget_password_window.title("Reset Passwords")
-    forget_password_window.geometry("300x200")
-    forget_password_window.minsize(300, 200)
-    forget_password_window.maxsize(300, 200)
-    tk.Label(forget_password_window,text="Enter pet name : ").place(x=1,y=10)
-    recovery_pet_name=ttk.Entry(forget_password_window)
-    recovery_pet_name.place(x=90,y=10)
-    button_var=ttk.Button(forget_password_window,text="Enter",command=show_current_pass)
-    button_var.place(x=220,y=10)
+# def forget_password():
+#     forget_password_window = Tk()
+#     forget_password_window.title("Reset Passwords")
+#     forget_password_window.geometry("300x200")
+#     forget_password_window.minsize(300, 200)
+#     forget_password_window.maxsize(300, 200)
+#     tk.Label(forget_password_window,text="Enter pet name : ").place(x=1,y=10)
+#     recovery_pet_name=ttk.Entry(forget_password_window)
+#     recovery_pet_name.place(x=90,y=10)
+#     button_var=ttk.Button(forget_password_window,text="Enter",command=show_current_pass)
+#     button_var.place(x=220,y=10)
 
-def show_current_pass():pass
+# def show_current_pass():pass
 
 def write_password_to_file():
 
@@ -75,9 +75,9 @@ def cheak_acess_key():
         AccessKey.delete(0, END)
         ttk.Label(main, text="wrong access key",
                   foreground='red').place(y=180, x=100)
-        button_var = Button(main, text="Forget Password", height=1,
-                            borderwidth=0, fg="blue", font=('Helvetica 9 underline'),command=forget_password)
-        button_var.place(x=0, y=120)
+        # button_var = Button(main, text="Forget Password", height=1,
+        #                     borderwidth=0, fg="blue", font=('Helvetica 9 underline'),command=forget_password)
+        # button_var.place(x=0, y=120)
 
 
 def set_new_acess_key(): 
@@ -168,9 +168,9 @@ if __name__ == "__main__":
     if read_file("key.txt") == '':
         PasswordSetUp = ttk.Button(main, text="Set Up The Access Key", command=change, width=50)
         PasswordSetUp.place(y=100, x=0)
-        ttk.Label(settings, text="Petname (recovery) : ").place(y=110, x=1)
-        PasswordRecovertAnswer = ttk.Entry(settings)
-        PasswordRecovertAnswer.place(y=110, x=150)
+        # ttk.Label(settings, text="Petname (recovery) : ").place(y=110, x=1)
+        # PasswordRecovertAnswer = ttk.Entry(settings)
+        # PasswordRecovertAnswer.place(y=110, x=150)
 
     button_var = ttk.Button(settings, text="change access password",command=set_new_acess_key)
     button_var.place(x=140, y=170)
---
+++ source_code/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+        # ttk.Label(settings, text="Petname (recovery) : ").place(y=110, x=1)
+        # PasswordRecovertAnswer = ttk.Entry(settings)
+        # PasswordRecovertAnswer.place(y=110, x=150)
