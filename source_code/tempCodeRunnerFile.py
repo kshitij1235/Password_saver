526baac19dbc919c305ecfc1e620f1a1b084236a
Kshitij <65331304+kshitij1235@users.noreply.github.com>
2021-10-24 22:30:41 +0530

    Readme_media

--- source_code/tempCodeRunnerFile.py
+++ source_code/tempCodeRunnerFile.py
@@ -1,3 +1,1 @@
-        # ttk.Label(settings, text="Petname (recovery) : ").place(y=110, x=1)
-        # PasswordRecovertAnswer = ttk.Entry(settings)
-        # PasswordRecovertAnswer.place(y=110, x=150)
+cryptography
