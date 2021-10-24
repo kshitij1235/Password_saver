c305e02516e0a23d373c71d6f98404195fc3e3be
Kshitij <65331304+kshitij1235@users.noreply.github.com>
2021-10-24 18:59:42 +0530

    solved some major bugs

--- source_code/password.py
+++ source_code/password.py
@@ -1,6 +1,11 @@
+from functools import partial
+from os import access, read
 import tkinter as tk
 from tkinter import Button, Entry, Label, Text, Tk, ttk
+import tkinter
 from tkinter.constants import END
+from types import prepare_class
+from typing import cast
 
 
 def write_file(filename, content, method):
@@ -12,22 +17,25 @@ def write_file(filename, content, method):
         pass
 
 
+
 def read_file(filename):
     try:
-        with open(str(filename), "r") as file:
-            return str(file.read())
+        with open(str(filename), "r+") as file:
+            content=str(file.read())
+            file.close()
+            return content
     except:
         pass
 
 
-# def encrypt_data(data):
-#     from cryptography.fernet import Fernet
-#     key = Fernet.generate_key()
-#     fernet = Fernet(key)
-#     enc_message = fernet.encrypt(data.encode())
-#     print(enc_message)
-#     # decMessage = fernet.decrypt(encMessage).decode()
-#     # print("decrypted string: ", decMessage)
+def encrypt_data(data):
+    from cryptography.fernet import Fernet
+    key = Fernet.generate_key()
+    fernet = Fernet(key)
+    enc_message = fernet.encrypt(data.encode())
+    print(enc_message)
+    # decMessage = fernet.decrypt(encMessage).decode()
+    # print("decrypted string: ", decMessage)
 
 
 def display_passwords():
@@ -42,19 +50,31 @@ def display_passwords():
 
     output.insert(END, read_file('data.txt'))
 
-# def forget_password():
-#     forget_password_window = Tk()
-#     forget_password_window.title("Reset Passwords")
-#     forget_password_window.geometry("300x200")
-#     forget_password_window.minsize(300, 200)
-#     forget_password_window.maxsize(300, 200)
-#     tk.Label(forget_password_window,text="Enter pet name : ").place(x=1,y=10)
-#     recovery_pet_name=ttk.Entry(forget_password_window)
-#     recovery_pet_name.place(x=90,y=10)
-#     button_var=ttk.Button(forget_password_window,text="Enter",command=show_current_pass)
-#     button_var.place(x=220,y=10)
 
-# def show_current_pass():pass
+class password_recovery:
+    def forget_password():
+        forget_password_window = Tk()
+        forget_password_window.title("Reset Passwords")
+        forget_password_window.geometry("300x200")
+        forget_password_window.minsize(300, 200)
+        forget_password_window.maxsize(300, 200)
+        tk.Label(forget_password_window,
+                 text="Enter pet name : ").place(x=1, y=10)
+        recovery_pet_name = ttk.Entry(forget_password_window)
+        recovery_pet_name.place(x=90, y=10)
+        print(cast)
+        button_var = ttk.Button(forget_password_window, text="Enter",
+                                command=lambda: password_recovery.show_current_pass(recovery_pet_name.get()))
+        button_var.place(x=220, y=10)
+
+    def show_current_pass(revovery_pet_name):
+        try:
+            if read_file("recovery.txt")== revovery_pet_name:
+                write_file('recovery.txt', "", "w")
+                root.select(settings)
+        except:
+            pass
+
 
 def write_password_to_file():
 
@@ -68,33 +88,43 @@ def write_password_to_file():
 
 
 def cheak_acess_key():
-    if AccessKey.get() == read_file('key.txt'):
-        AccessKey.delete(0, END)
+
+    if AccessKey.get()==read_file("key.txt"):
         display_passwords()
-    elif read_file('key.txt') != '':
+    
+    else:
         AccessKey.delete(0, END)
         ttk.Label(main, text="wrong access key",
                   foreground='red').place(y=180, x=100)
-        # button_var = Button(main, text="Forget Password", height=1,
-        #                     borderwidth=0, fg="blue", font=('Helvetica 9 underline'),command=forget_password)
-        # button_var.place(x=0, y=120)
+        button_var = Button(main, text="Forget Password", height=1,
+                            borderwidth=0, fg="blue", font=('Helvetica 9 underline'), command=password_recovery.forget_password)
+        button_var.place(x=0, y=120)
 
 
-def set_new_acess_key(): 
-    if old_pass.get() == read_file("key.txt"):
+def set_new_acess_key():
+    if old_pass.get() ==read_file("key.txt"):
         if new_pass.get() == conf_pass.get():
             write_file("key.txt", str(conf_pass.get())+'\n', "w")
-            write_file("key.txt", PasswordRecovertAnswer.get(), "a")
-            ttk.Label(settings, text="Password Set",foreground='green').place(y=170, x=100)
-            try:PasswordSetUp.destroy()
-            except:pass
-        else:Label(settings, text="password doesnt match",foreground='red').place(y=140, x=30)
-    else:Label(settings, text="old key is wrong",foreground='red').place(y=140, x=30)
+            write_file("recovery.txt", PasswordRecovertAnswer.get(), "a")
+            ttk.Label(settings, text="Password Set",
+                      foreground='green').place(y=170, x=100)
+            try:
+                PasswordSetUp.destroy()
+            except:
+                pass
+        else:
+            Label(settings, text="password doesnt match",
+                  foreground='red').place(y=140, x=30)
+    else:
+        Label(settings, text="old key is wrong",
+              foreground='red').place(y=140, x=30)
     old_pass.delete(0, END)
     new_pass.delete(0, END)
     conf_pass.delete(0, END)
-    try:PasswordRecovertAnswer.delete(0, END)
-    except:pass
+    try:
+        PasswordRecovertAnswer.delete(0, END)
+    except:
+        pass
 
 
 def change():
@@ -102,13 +132,14 @@ def change():
 
 
 if __name__ == "__main__":
-
     root = tk.Tk()
 
     root.title("Password saver")
 
-    try:root.iconbitmap('.\icon.ico')
-    except:pass
+    try:
+        root.iconbitmap('.\icon.ico')
+    except:
+        pass
 
     root.geometry("300x230")
     root.minsize(300, 230)
@@ -144,10 +175,12 @@ if __name__ == "__main__":
     AccessKey = ttk.Entry(main, show='*')
     AccessKey.place(y=100, x=150)
 
-    button_var = ttk.Button(main, text="submit",command=write_password_to_file)
+    button_var = ttk.Button(main, text="submit",
+                            command=write_password_to_file)
     button_var.place(x=50, y=150)
 
-    button_var = ttk.Button(main, text="show password",command=cheak_acess_key)
+    button_var = ttk.Button(main, text="show password",
+                            command=cheak_acess_key)
     button_var.place(x=140, y=150)
 
 #####################-settings-###############################
@@ -165,13 +198,16 @@ if __name__ == "__main__":
     conf_pass = ttk.Entry(settings, show="*")
     conf_pass.place(y=70, x=150)
 
-    if read_file("key.txt") == '':
-        PasswordSetUp = ttk.Button(main, text="Set Up The Access Key", command=change, width=50)
-        PasswordSetUp.place(y=100, x=0)
-        # ttk.Label(settings, text="Petname (recovery) : ").place(y=110, x=1)
-        # PasswordRecovertAnswer = ttk.Entry(settings)
-        # PasswordRecovertAnswer.place(y=110, x=150)
 
-    button_var = ttk.Button(settings, text="change access password",command=set_new_acess_key)
+    if read_file("recovery.txt")=="" and read_file("key.txt")=="":
+        PasswordSetUp = ttk.Button(
+            main, text="Set Up The Access Key", command=change, width=50)
+        PasswordSetUp.place(y=100, x=0)
+        ttk.Label(settings, text="Petname (recovery) : ").place(y=110, x=1)
+        PasswordRecovertAnswer = ttk.Entry(settings, show="*")
+        PasswordRecovertAnswer.place(y=110, x=150)
+
+    button_var = ttk.Button(
+        settings, text="change access password", command=set_new_acess_key)
     button_var.place(x=140, y=170)
     root.mainloop()
